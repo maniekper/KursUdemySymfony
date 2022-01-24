da787ace9b03623b50f71080e518e6b663093631
maniekper <maniekper@o2.pl>
2022-01-24 11:36:14 +0100

    dodanie wszystkich kontrolerów i przetworzenie plików widoku z php na ywig

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -13,4 +13,46 @@ class FrontController extends AbstractController
     {
         return $this->render('front/index.html.twig');
     }
+
+    #[Route('/video-list', name: 'video_list')]
+    public function videoList(): Response
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    #[Route('/video-detalis', name: 'video_details')]
+    public function videoDetalis(): Response
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    #[Route('/search-results', methods:['POST'] , name: 'search_results')]
+    public function searchResults(): Response
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    #[Route('/pricing', name: 'pricing')]
+    public function pricing(): Response
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    #[Route('/payment', name: 'payment')]
+    public function payment(): Response
+    {
+        return $this->render('front/payment.html.twig');
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
